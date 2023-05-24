22ebaff05df5a5607edf39cc057e627274f98bb7
Juliana Guerreiro <126183294+juliana242@users.noreply.github.com>
2023-05-24 10:15:42 -0300

    Create resultados.php

---
+++ resultados.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividades</title>
+    <link rel="stylesheet" href="css/paquetes.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+      <!--Icons from Freepik -  www.flaticon.com--> 
+
+</head>
+<body>
+    <header id="navbar">
+        <nav class="navbar-container container">
+          <a href="inicio.html">
+            <img src="css/imagenes/logoblanco.png" alt="Logo ArgenPlanner" class="navbar-logo">
+          </a>
+          <button
+            type="button"
+            id="navbar-toggle"
+            aria-controls="navbar-menu"
+            aria-label="Toggle menu"
+            aria-expanded="false"
+          >
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <div id="navbar-menu" aria-labelledby="navbar-toggle">
+            <ul class="navbar-links">
+              <li class="navbar-item"><a class="navbar-link" href="inicio.html">Inicio</a></li>
+              <li class="navbar-item"><a class="navbar-link" href="inicio.html#somos-argenplanner">Sobre Nosotros</a></li>
+              <li class="navbar-item"><a class="navbar-link" href="inicio.html#caracteristicas">Caracteristicas</a></li>
+              <li class="navbar-item"><a class="navbar-link" href="actividades.html">Actividades</a></li>
+              <li class="navbar-item"><a class="navbar-link" href="paquetes.html">Paquetes</a></li>
+              <li class="navbar-item"><a class="navbar-link" href="contacto.html">Contacto</a></li>
+            </ul>
+          </div>
+        </nav>
+    </header>
+<h1>Esta pagina mostrara los resultados de busqueda</h1>
+    <div class="paquetes">
+    <section id="bariloche">
+        <p class="descripcion"> <span style="font-weight:bolder">Bariloche <br> Vuelo + Hotel!</span><br> Mayo 2023 - 12 noches <br> Vuelo + Alojamiento Hotel Concept <br> Precio por persona desde 300.010 </p>
+    </section>
+    <section id="sanMartin">
+        <p class="descripcion"> <span style="font-weight:bolder">San Martin de los Andes <br> Vuelo + Hotel!</span> <br> Mayo 2023 - 10 noches <br> Vuelo + Alojamiento Hotel Concept <br> Precio por persona desde 200.010 </p>
+    </section>
+    <section id="villaAng">
+        <p class="descripcion"> <span style="font-weight:bolder">Villa Angostura <br> Vuelo + Hotel!</span> <br> Mayo 2023 - 12 noches <br> Vuelo + Alojamiento Hotel Concept <br> Precio por persona desde 400.020 </p>
+    </section>
+</div>
+
+</body>
+
+<div class="footer">
+    <footer class="container">
+      <div class="footer-sections">
+        <div class="somos-footer">
+          <h3>Somos ArgenPlanner</h3>
+        <p>Queremos que todos puedan volar de la manera más rápida y accesible posible. Somos una empresa que ofrece paquetes turísticos en Argentina para que puedas disfrutar de tus vacaciones sin preocuparte por la organización del viaje.</p>
+      </div>
+        
+      <div class="nosotros-footer">
+        <h3>Nosotros</h3>
+        <ul>
+          <li><a href="#somos-argenplanner">Sobre ArgenPlanner</a></li>
+          <li><a href="trabaja.html">Sumate a nuestro equipo</a></li>
+          <li><a href="contacto.html">¿Preguntas?</a></li>
+        </ul>
+      </div>
+      <div class="arma-footer">
+        <h3>Arma tu viaje</h3>
+        <ul>
+          <li><a href="Destinos.html"> Destinos</a></li>
+          <li><a href="Aeropuertos.html"> Aeropuertos</a></li>
+        </ul>
+      </div>
+      </div>
+      <div class="socialIcon">
+        <img src="css/imagenes/FACEBOOK ICONO.png" alt="Facebook Icon" class="icon">
+        <img src="css/imagenes/TWITTER ICONO.png" alt="Twitter Icon" class="icon">
+        <img src="css/imagenes/linkedin_square_icon_160786.png" alt="LI Icon" class="icon">
+        <img src="css/imagenes/instagram icono.png" alt="Instagram Icon" class="icon">
+      </div>
+    </footer>
+  </div>
+  <script src="js/inicio.js"></script>
+</html>
